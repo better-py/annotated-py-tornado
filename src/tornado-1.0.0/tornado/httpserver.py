da397de05bb9656a679e26de2b3ec26b73f72179
hhstore <hhstore@qq.com>
2016-01-22 16:53:52 +0800

    update: 更新 对 httpserver 模块的注解.

--- src/tornado-1.0.0/tornado/httpserver.py
+++ src/tornado-1.0.0/tornado/httpserver.py
@@ -1,20 +1,10 @@
 #!/usr/bin/env python
-#
-# Copyright 2009 Facebook
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
+# -*- coding: utf8 -*-
 
-"""A non-blocking, single-threaded HTTP server."""
+
+"""A non-blocking, single-threaded HTTP server.
+    非阻塞,单线程 HTTP 服务器
+"""
 
 import cgi
 import errno
@@ -30,15 +20,16 @@ import urlparse
 try:
     import fcntl
 except ImportError:
-    if os.name == 'nt':
+    if os.name == 'nt':     # 判断是否是 Windows 内核
         import win32_support as fcntl
     else:
         raise
 
 try:
-    import ssl # Python 2.6+
+    import ssl  # Python 2.6+
 except ImportError:
     ssl = None
+
 
 class HTTPServer(object):
     """A non-blocking, single-threaded HTTP server.
